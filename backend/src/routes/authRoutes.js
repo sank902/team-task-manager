3334219649d176a84c5fe7eebae4525e03c3a15a
Sankalp <kharesankalp57@gmail.com>
2026-05-08 17:54:49 +0530

    auth

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -3,7 +3,7 @@ import { signup, login } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
+router.post("/register", signup);
 router.post("/login", login);
 
 export default router;
